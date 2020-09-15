5a465b8736c1d2f9302c3c408b226678a4bd4e92
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-09-15 00:41:53 -0500

    Configurando Express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hola Mundo');
+// Configuración básica de Express
+const express = require('express');
+
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get('/', (req, res) => { // Petición Get
+    res.json({
+        ok: true
+    });
+});
+
+// Escuchar Peticiones
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en puerto 4000`);
+});
