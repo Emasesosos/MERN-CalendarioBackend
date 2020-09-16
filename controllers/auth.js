2128791ef2a33d07fed3740d5e8b91af00ec421c
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-09-16 13:59:13 -0500

    Encriptar la contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,18 +5,30 @@ const Usuario = require('../models/Usuario');
 // Crear Usuario
 const crearUsuario = async(req, res = response) => {
 
-    // const { name, email, password } = req.body;
+    const { name, email, password } = req.body;
 
     try {
-        const usuario = new Usuario(req.body);
+
+        let usuario = await Usuario.findOne({ email: email });
+        // console.log(usuario);
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario existe con ese correo'
+            });
+        }
+
+        usuario = new Usuario(req.body);
         await usuario.save();
 
         res.status(201).json({
             ok: true,
-            msg: 'Register',
+            uid: usuario.id,
+            name: usuario.name
         });
+
     } catch (error) {
-        console.log(error);
+        // console.log(error);
         res.status(500).json({
             ok: false,
             msg: 'Por favor hable con el Administrador'
