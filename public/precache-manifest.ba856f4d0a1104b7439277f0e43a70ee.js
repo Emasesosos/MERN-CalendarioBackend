07792d1f5fa90a703e0d996d0d081e1d99f21a74
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-09-29 01:51:05 -0500

    v1 CalendarApp

---
+++ public/precache-manifest.ba856f4d0a1104b7439277f0e43a70ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5dcf3c44890ef30043df5e8d83dcf42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3da7770d8f57ceefea32",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "7d1f460f83a575780dc2",
+    "url": "/static/css/main.84d39ae1.chunk.css"
+  },
+  {
+    "revision": "3da7770d8f57ceefea32",
+    "url": "/static/js/2.760841af.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.760841af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1f460f83a575780dc2",
+    "url": "/static/js/main.542f487a.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
